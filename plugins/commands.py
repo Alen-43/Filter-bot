41f984f74041c34ecbb2d64e66c0963b0529ec2d
Alen-43 <98441795+Alen-43@users.noreply.github.com>
2022-05-27 11:53:41 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -103,8 +103,7 @@ async def start(client, message):
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
-            parse_mode='html'
-        )
+            )
         return
     data = message.command[1]
     try:
